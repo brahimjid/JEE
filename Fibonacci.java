0140326ed06a8bd3fc409a8413c425cab6f42f33
Brahim jid <brmed94@gmail.com>
2019-11-13 22:35:46 +0000

    Fibonacci recursive

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,18 +1,18 @@
 
 public class Fibonacci {
+	  static int fib(int n) 
+	    { 
+	        if (n <= 1) 
+	            return n; 
+	        return fib(n - 1) + fib(n - 2); 
+	    }
 
 	public static void main(String[] args) {		
-		int n1=0,n2=1,n3,i,count=10;    
-		 System.out.print(n1+" "+n2);
-		    
-		 for(i=2;i<count;++i)  
-		 {    
-		  n3=n1+n2;    
-		  System.out.print(" "+n3);    
-		  n1=n2;    
-		  n2=n3;    
-	}
-		 
+		int n = 10;
+		System.out.print("Series : ");
+		for(int i = 0; i < n; i++){
+				System.out.print(fib(i) +" ");
+			}
 	}
 }
 		 
